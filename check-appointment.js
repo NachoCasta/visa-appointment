05efb2f2891c4181e1e0272d563709caae7459ef
Ignacio Castañeda <icasta@fb.com>
2024-06-01 17:18:42 +0200

    [fix] March 2024 update

--- check-appointment.js
+++ check-appointment.js
@@ -8,6 +8,9 @@ import puppeteer from "puppeteer";
 import fs from "fs";
 import * as path from "path";
 import { fileURLToPath } from "url";
+import PlaySound from "play-sound";
+
+const player = PlaySound();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -38,7 +41,7 @@ clear();
 
 console.log(chalk.yellow(figlet.textSync("VISA Appointment")));
 
-const interval = 10 * 60 * 1000; // 10 minutes
+const interval = 90 * 60 * 1000; // 10 minutes
 
 const alertBefore = new Date(config.alert_for_appointment_before);
 
@@ -48,40 +51,100 @@ let checkAvailability = () => {
   (async () => {
     gSpinner.stop();
     console.log(chalk.gray("Opening Chrome headless..."));
-    let browser = await puppeteer.launch();
+    let browser = await puppeteer.launch({
+      headless: true,
+      defaultViewport: {
+        width: 1300,
+        height: 1080,
+      },
+    });
     let page = await browser.newPage();
 
     let spinner = new CLI.Spinner("Signing in...");
     spinner.start();
+    try {
+      await page.goto("https://ais.usvisa-info.com/es-cl/niv/users/sign_in");
+      await page.type("#user_email", config.username);
+      await page.type("#user_password", config.password);
+      await page.$eval("#policy_confirmed", (check) => (check.checked = true));
+      // await page.waitForTimeout(3000);
+      await Promise.all([
+        page.waitForNavigation(),
+        page.click("input[type=submit]"),
+      ]).catch(() => {
+        console.error("Error Occurred.");
+      });
+      spinner.stop();
+      console.log(chalk.green("Signed in!"));
+      console.log(chalk.yellow(`Checking at: ${Date().toLocaleString()}`));
 
-    await page.goto("https://ais.usvisa-info.com/es-cl/niv/users/sign_in");
-    await page.type("#user_email", config.username);
-    await page.type("#user_password", config.password);
-    await page.$eval("#policy_confirmed", (check) => (check.checked = true));
-    await page.waitForTimeout(3000);
-    await Promise.all([
-      page.waitForNavigation(),
-      page.click("input[type=submit]"),
-    ]).catch(() => {
-      console.error("Error Occurred.");
-    });
-    spinner.stop();
-    console.log(chalk.green("Signed in!"));
-    console.log(chalk.yellow(`Checking at: ${Date().toLocaleString()}`));
+      // Click "Continuar"
+      // Esto puede fallar dependiendo del idioma del computador,
+      // en ese caso cambiar a 'Continue'
+      await page.click("xpath=//a[contains(text(), 'Continuar')]");
+      await sleep(3000);
 
-    let response = await page.goto(
-      `https://ais.usvisa-info.com/es-cl/niv/schedule/${config.schedule_id}/appointment/days/${locationId}.json?appointments`
-    );
-    let json = await response.json();
-    console.log(json.slice(0, 5));
-    if (json.length == 0) {
-      console.log(chalk.red("No appointments!"));
-    } else if (Date.parse(json[0].date) < alertBefore) {
-      const text = `Hora disponible el: ${json[0].date}`;
-      console.log(chalk.green(text));
-      sendMessage(text);
-    } else {
-      console.log(chalk.red("No early appointments!"));
+      // Click "Reprogramar cita"
+      const accordionItems = await page.$$(".accordion-item");
+      accordionItems[2].click();
+      await sleep(1000);
+      await page.click(".accordion-item:nth-child(3) .small-only-expanded");
+      await sleep(3000);
+
+      // Click "Continuar"
+      await page.click("input[type=submit]");
+      await sleep(4000);
+
+      try {
+        // Click "Fecha de la cita"
+        const dateInputSelector = "#appointments_consulate_appointment_date";
+        await page.waitForSelector(dateInputSelector);
+        const dateInput = await page.$(dateInputSelector);
+        await dateInput.click();
+      } catch (error) {
+        throw new NotAvailableError();
+      }
+
+      // Find available dates
+      let availableDates = [];
+      while (availableDates.length === 0) {
+        availableDates = await page.$$(
+          "#ui-datepicker-div tbody td:not(.ui-datepicker-unselectable)"
+        );
+        if (availableDates.length === 0) {
+          await page.click(".ui-datepicker-next");
+        }
+      }
+
+      // Select earliest date
+      await availableDates[0].click();
+      await sleep(1000);
+
+      // Check if it's earlier than current
+      await page.waitForSelector(dateInputSelector);
+      const dateString = await page.evaluate(
+        (element) => element.value,
+        dateInput
+      );
+      const date = new Date(dateString);
+      if (date < alertBefore) {
+        const text = `Hora disponible el: ${dateString}`;
+        console.log(chalk.green(text));
+        // player.play("alarm.wav");
+        sendMessage(text);
+      } else {
+        console.log(
+          chalk.red(
+            `No hay disponibilidad antes de la fecha actual. Próxima disponibilidad: ${dateString}`
+          )
+        );
+      }
+    } catch (error) {
+      let errorMessage = "Hubo un error, se intentará denuevo";
+      if (error instanceof NotAvailableError) {
+        errorMessage = "No hay citas disponibles, se intentará denuevo";
+      }
+      console.log(chalk.red(errorMessage));
     }
     console.log(chalk.gray("Closing Chrome headless..."));
     await browser.close();
@@ -104,3 +167,8 @@ const sendMessage = (text) =>
       )
     )
   );
+
+const sleep = (ms) =>
+  new Promise((resolve) => setTimeout(() => resolve(ms), ms));
+
+class NotAvailableError extends Error {}
